2911cd650322bcd5dcb5381dd127a4b9b64572ed
NourTabib <98242416+NourTabib@users.noreply.github.com>
2023-09-19 17:43:31 +0100

    added producer

--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -35,6 +35,62 @@ import java.util.logging.Logger;
 @EnableKafkaStreams
 public class Demo2Application {
 
+
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(Demo2Application::Produce);
+        thread.start();
+        System.out.println("Main thread is executing.");
+        Properties properties1 = new Properties();
+        properties1.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-test-8");
+        properties1.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        properties1.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties1.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG,4);
+        properties1.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        properties1.put("transaction.state.log.replication.factor",1);
+        properties1.put("schema.registry.url", "http://localhost:8081");
+        final Serde<TransactionEventAvro> TRANSACTION_SERDE = new SpecificAvroSerde<>();
+        final Serde<List<TransactionEventAvro>> LISTE_SERDE = new Serdes.ListSerde(ArrayList.class,TRANSACTION_SERDE);
+        LISTE_SERDE.configure(Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081"),false);
+        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081");
+        TRANSACTION_SERDE.configure(serdeConfig, false);
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, TransactionEventAvro> stream = builder.stream("transactions.events",Consumed.with(Serdes.String(),TRANSACTION_SERDE));
+        stream.peek((key, value) -> {
+
+        });
+        KGroupedStream<String,TransactionEventAvro> eventAvroKGroupedStream = stream
+                .map((key,value)-> new KeyValue<>(value.getAccountNumber().toString(),value))
+                .groupBy(
+                        (key,value) -> key ,
+                        Grouped.with(Serdes.String(),TRANSACTION_SERDE)
+                );
+        KTable<String,List<TransactionEventAvro>> kTable = eventAvroKGroupedStream
+                .aggregate(
+                        ArrayList<TransactionEventAvro>::new,
+                        (key,value,list) -> {
+                            list.add(value);
+                            return list;
+                        },
+                        Materialized.<String,List<TransactionEventAvro>,KeyValueStore<Bytes,byte[]>>as("GROUPED-KTABLE-EVENTS-STORE-2")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(LISTE_SERDE)
+                                //with(Serdes.String(),LISTE_SERDE)
+                );
+        kTable.toStream()
+                //.peek((key,value) -> System.out.println("GROUPED STREAM PEEK || " +key + " : " + value.get(0).getAtmAddress() + value.get(1).getAtmAddress() + "\n"))
+                .to("transactions.events.grouped",Produced.with(Serdes.String(),LISTE_SERDE));
+
+        builder.stream("transactions.events.grouped",Consumed.with(Serdes.String(),LISTE_SERDE))
+                .map((key,value)-> new KeyValue<>(key,value.toString()))
+                .peek((key,value) -> System.out.println("Key : "+ key + " Value: " + value))
+                .to("transactions.events.output",Produced.with(Serdes.String(),Serdes.String()));
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(),properties1);
+        kafkaStreams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+    }
     public static void Produce(){
         List<String> addressList = Arrays.asList(
                 "Rue de Rome, Tunis",
@@ -140,60 +196,4 @@ public class Demo2Application {
             }
         }
     }
-
-    public static void main(String[] args) {
-        Thread thread = new Thread(Demo2Application::Produce);
-        thread.start();
-        System.out.println("Main thread is executing.");
-        Properties properties1 = new Properties();
-        properties1.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-test-8");
-        properties1.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        properties1.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        properties1.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG,4);
-        properties1.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-        properties1.put("transaction.state.log.replication.factor",1);
-        properties1.put("schema.registry.url", "http://localhost:8081");
-        final Serde<TransactionEventAvro> TRANSACTION_SERDE = new SpecificAvroSerde<>();
-        final Serde<List<TransactionEventAvro>> LISTE_SERDE = new Serdes.ListSerde(ArrayList.class,TRANSACTION_SERDE);
-//        LISTE_SERDE.configure(Collections.singletonMap("schema.registry.url",
-//                "http://localhost:8081"),false);
-        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
-                "http://localhost:8081");
-        TRANSACTION_SERDE.configure(serdeConfig, false);
-        StreamsBuilder builder = new StreamsBuilder();
-//        KStream<String, TransactionEventAvro> stream = builder.stream("transactions.events",Consumed.with(Serdes.String(),TRANSACTION_SERDE));
-//        stream.peek((key, value) -> {
-//
-//        });
-//        KGroupedStream<String,TransactionEventAvro> eventAvroKGroupedStream = stream
-//                .map((key,value)-> new KeyValue<>(value.getAccountNumber().toString(),value))
-//                .groupBy(
-//                        (key,value) -> key ,
-//                        Grouped.with(Serdes.String(),TRANSACTION_SERDE)
-//                );
-//        KTable<String,List<TransactionEventAvro>> kTable = eventAvroKGroupedStream
-//                .aggregate(
-//                        ArrayList<TransactionEventAvro>::new,
-//                        (key,value,list) -> {
-//                            list.add(value);
-//                            return list;
-//                        },
-//                        Materialized.<String,List<TransactionEventAvro>,KeyValueStore<Bytes,byte[]>>as("GROUPED-KTABLE-EVENTS-STORE-2")
-//                                .withKeySerde(Serdes.String())
-//                                .withValueSerde(LISTE_SERDE)
-//                                //with(Serdes.String(),LISTE_SERDE)
-//                );
-//        kTable.toStream()
-//                //.peek((key,value) -> System.out.println("GROUPED STREAM PEEK || " +key + " : " + value.get(0).getAtmAddress() + value.get(1).getAtmAddress() + "\n"))
-//                .to("transactions.events.grouped",Produced.with(Serdes.String(),LISTE_SERDE));
-
-//        builder.stream("transactions.events.grouped",Consumed.with(Serdes.String(),LISTE_SERDE))
-//                .map((key,value)-> new KeyValue<>(key,value.toString()))
-//                .peek((key,value) -> System.out.println("Key : "+ key + " Value: " + value))
-//                .to("transactions.events.output",Produced.with(Serdes.String(),Serdes.String()));
-//        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(),properties1);
-//        kafkaStreams.start();
-//        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
-    }
-
 }
